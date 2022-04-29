38932da10607a342571041c804a8bd373e61d5e6
Michael Acero <michaelacero@MacBook-Air-de-Michael.local>
2022-04-29 11:58:35 -0500

    envio

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/conovic'));
+app.use(express.static('./dist/spotyfy'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/conovic/'}),
+    res.sendFile('index.html', {root: 'dist/spotyfy/'}),
 );
 
 // Start the app by listening on the default Heroku port
