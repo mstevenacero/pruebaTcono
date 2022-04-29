1775619cabf37ed653cb4abe149e2f165ddad2ab
Michael Acero <michaelacero@MacBook-Air-de-Michael.local>
2022-04-29 12:53:33 -0500

    cambios

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/spotyfy'));
+app.use(express.static('./dist/conovic'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/spotyfy/'}),
+    res.sendFile('index.html', {root: 'dist/conovic/'}),
 );
 
 // Start the app by listening on the default Heroku port
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   BACK_URL: 'https://608adc0d737e470017b7410f.mockapi.io/api/v1/',
 };
 
